454a5eb96f892aae89ebc1390c2795454a8f3870
Pavel Khokhlov <pkhokhlov@naumen.ru>
2022-11-28 23:51:35 +0300

    update

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -21,7 +21,6 @@ const Form = () => {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
-                'Access-Control-Allow-Origin':'*'
             },
             body: JSON.stringify(data)
         });
