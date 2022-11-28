c4a606692c377262b00ccfd3ecfb822c312012ac
Pavel Khokhlov <pkhokhlov@naumen.ru>
2022-11-28 22:48:00 +0300

    update

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -6,7 +6,7 @@ const Form = () => {
     const [country, setCountry] = useState('');
     const [street, setStreet] = useState('');
     const [subject, setSubject] = useState('physical');
-    const {tg, queryId} = useTelegram();
+    const {tg} = useTelegram();
 
     const onSendData = useCallback(() => {
         const data = {
